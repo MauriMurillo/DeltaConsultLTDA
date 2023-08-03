d142f571873fc32924800aaec457ee75673c115b
MauricioMurillo <mauricio.murillo.gs@gmail.com>
2023-08-03 10:59:48 -0400

    services Desktop

--- src/screens/Services/Services.js
+++ src/screens/Services/Services.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Services.css";
 import { Header } from "../../components/Header/Header.js";
 import { Footer } from "../../components/Footer/Footer.js";
 
@@ -6,10 +7,42 @@ function Services() {
   return (
     <div className="servicesScreen">
       <Header />
-      <div className="servicesSection" style={{marginTop: "120px"}}>
-
+      <div className="servicesSection">
+        <div className="textContainer">
+          <p className="titleServices">SERVICIOS</p>
+          <p className="textServices">
+            Ofrecemos siempre las mejores soluciones
+          </p>
+        </div>
+        <div className="serviceGrid">
+          <div className="serviceA">
+            <div>
+              <p>AUDITORIA EXTERNA</p>
+            </div>
+          </div>
+          <div className="serviceB">
+            <div>
+              <p>CONSULTORIA IMPOSITIVA</p>
+            </div>
+          </div>
+          <div className="serviceC">
+            <div>
+              <p>CONSULTORIA ESTRATEGICA</p>
+            </div>
+          </div>
+          <div className="serviceD">
+            <div>
+              <p>CONSULTORIA FINANCIERA</p>
+            </div>
+          </div>
+          <div className="serviceE">
+            <div>
+              <p>CONSULTORIA DE SOSTENIBILIDAD</p>
+            </div>
+          </div>
+        </div>
       </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
