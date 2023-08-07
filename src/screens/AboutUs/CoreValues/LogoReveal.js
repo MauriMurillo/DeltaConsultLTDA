a9c917a697a4e7d75f5e9dd70c76aaa45c9c5d2a
MauricioMurillo <mauricio.murillo.gs@gmail.com>
2023-08-07 07:12:55 -0400

    values mobile

--- src/screens/AboutUs/CoreValues/LogoReveal.js
+++ src/screens/AboutUs/CoreValues/LogoReveal.js
@@ -1,16 +1,19 @@
 import React from "react";
 import { easeOut, motion } from "framer-motion";
+import { useContext } from "react";
+import { DeltaContext } from "../../../Contexts/DeltaContext";
 function LogoReveal(props) {
   const { principal, secondary } = props;
+  const { screenSize } = useContext(DeltaContext);
   return (
     <div className="logoRevealContainer">
       <motion.div
-        whileInView={{ x: 292 }}
+        whileInView={screenSize.width > 600 ? { x: 292 } : { x: 228 }}
         transition={{ ease: easeOut, duration: 1 }}
         className="halfLogoRight"
       ></motion.div>
       <motion.div
-        whileInView={{ opacity: [0,1] }}
+        whileInView={{ opacity: [0, 1] }}
         transition={{ duration: 1 }}
         className="card first"
       >
@@ -18,13 +21,13 @@ function LogoReveal(props) {
         <div className="cardDescription">{principal.description}</div>
       </motion.div>
       <motion.div
-        whileInView={{ x: -292 }}
+        whileInView={screenSize.width > 600 ? { x: -292 } : { x: -228 }}
         transition={{ ease: easeOut, duration: 1 }}
         className="halfLogoLeft"
       ></motion.div>
       <motion.div
         className="card second"
-        whileInView={{ opacity: [0,1] }}
+        whileInView={{ opacity: [0, 1] }}
         transition={{ duration: 1 }}
       >
         <div className="cardTitle">{secondary.title}</div>
