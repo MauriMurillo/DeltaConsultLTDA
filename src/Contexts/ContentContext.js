edce9d0e2060d1c5873410449350d00ee863b73e
MauricioMurillo <mauricio.murillo.gs@gmail.com>
2023-08-08 04:25:41 -0400

    contact desktop

--- src/Contexts/ContentContext.js
+++ src/Contexts/ContentContext.js
@@ -14,6 +14,18 @@ function ContentProvider(props) {
       telefono: "(+591-2) 2427222",
       mail: "deltaconsult@dcl.com.bo",
       mapLink: "https://goo.gl/maps/5Y46ZDYoUQtQL92x5",
+      embed: (
+        <iframe
+          title="Delta La Paz"
+          src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15300.869291984887!2d-68.1305497!3d-16.5151226!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x915f20886e3b40bd%3A0x33c60274bab7a6!2sDelta%20Consult%20Ltda.!5e0!3m2!1ses-419!2sbo!4v1691480311681!5m2!1ses-419!2sbo"
+          width="600"
+          height="450"
+          style={{ border: 0 }}
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+        ></iframe>
+      ),
     },
     {
       ciudad: "SANTA CRUZ",
@@ -21,6 +33,18 @@ function ContentProvider(props) {
       telefono: "(+591-3) 3454756",
       mail: "deltascz@dcl.com.bo",
       mapLink: "https://goo.gl/maps/ZBkzaSGN7K3RDGkf9",
+      embed: (
+        <iframe
+          title="Delta Santa Cruz"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3799.6720228623267!2d-63.1968408!3d-17.7600864!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x93f1e7f3fa740101%3A0xa8f15c0904502987!2sDelta%20Consult%20Ltda.!5e0!3m2!1ses-419!2sbo!4v1691480277691!5m2!1ses-419!2sbo"
+          width="600"
+          height="450"
+          style={{ border: 0 }}
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+        ></iframe>
+      ),
     },
     {
       ciudad: "COCHABAMBA",
@@ -29,6 +53,18 @@ function ContentProvider(props) {
       telefono: "(591)-72044457",
       mail: "zvillca@dcl.com.bo",
       mapLink: "https://goo.gl/maps/NUT77m3tGa78EmKr9",
+      embed: (
+        <iframe
+          title="Delta Cochabamba"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3807.9339042687793!2d-66.17080580000001!3d-17.3669132!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x93e3753b4acb8d53%3A0xed5f6ce2391c54d8!2sEdificio%20delta%20consult%20ltda!5e0!3m2!1ses-419!2sbo!4v1691480171701!5m2!1ses-419!2sbo"
+          width="600"
+          height="450"
+          style={{ border: 0 }}
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+        ></iframe>
+      ),
     },
   ];
 
@@ -113,7 +149,7 @@ function ContentProvider(props) {
         homeHeroContent,
         generalStatistics,
         clientStatistics,
-        trustStatement
+        trustStatement,
       }}
     >
       {props.children}
--- src/screens/Contact/BigOffice.js
+++ src/screens/Contact/BigOffice.js
@@ -1,11 +1,42 @@
 import React from "react";
-
+import emailIcon from "../../Assets/Icons/contact/Message.svg";
+import locationIcon from "../../Assets/Icons/contact/Location.svg";
+import phoneIcon from "../../Assets/Icons/contact/phone-icon.svg";
+import "./BigOffice.css";
 function BigOffice(props) {
   const { content } = props;
   return (
     <div className="bigOfficeContainer">
       <div className="officeHeading">
         <p>{content.ciudad}</p>
+      </div>
+      <div className="contentFlex">
+        <div className="officeDataContainer">
+          <p className="cityBig ">OFICINA {content.ciudad}</p>
+          <div className="cnb">
+            <a target="_blank" rel="noreferrer" href={"mailto:" + content.mail}>
+              <img src={emailIcon} className="icon" alt="email" />
+              <p> {content.mail}</p>
+            </a>
+          </div>
+          <div className="cnb" id="ph">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href={"tel:" + content.telefono}
+            >
+              <img src={phoneIcon} className="icon" alt="phone" />
+              <p> {content.telefono}</p>
+            </a>
+          </div>
+          <div className="cnb">
+            <a target="_blank" rel="noreferrer" href={content.mapLink}>
+              <img src={locationIcon} className="icon" alt="location" />
+              <p> {content.direccion}</p>
+            </a>
+          </div>
+        </div>
+        <div className="embedMap">{content.embed}</div>
       </div>
     </div>
   );
