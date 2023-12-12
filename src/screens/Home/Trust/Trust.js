c25a7d6d8b762e56bd265297a688c3cc08f2f493
MauricioMurillo <mauricio.murillo.gs@gmail.com>
2023-12-12 14:46:13 -0700

    trust

--- src/screens/Home/Trust/Trust.js
+++ src/screens/Home/Trust/Trust.js
@@ -1,58 +1,59 @@
 import React, { useContext, useState, useEffect } from "react";
 import "./Trust.css";
 import { ContentContext } from "../../../Contexts/ContentContext";
+import derecha from "../../../Assets/Images/Logos/blue_right.png";
 function Trust() {
-  const [trustPosition, setTrustPosition] = useState(0);
+  // const [trustPosition, setTrustPosition] = useState(0);
   const { trustStatement, trustImages } = useContext(ContentContext);
 
-  const animationClassOut = "animate__fadeOut";
-  const animationClass = "animate__fadeIn";
-  useEffect(() => {
-    const imageTrust = document.getElementById("imageTrust");
-    if (imageTrust) {
-        imageTrust.classList.remove(animationClass);
-        imageTrust.classList.remove(animationClassOut);
-        imageTrust.classList.add(animationClass);
-    }
-    const next = (trustPosition + 1) % trustImages.length;
-    const move = setTimeout(() => {
-      if (imageTrust) {
-        imageTrust.classList.remove(animationClass);
-        imageTrust.classList.remove(animationClassOut);
-        imageTrust.classList.add(animationClassOut);
-        setTimeout(() => {
-          setTrustPosition(next);
-        }, 801);
-      }
-    }, 7000);
-    return () => clearTimeout(move);
-  }, [trustPosition, trustImages]);
+  // const animationClassOut = "animate__fadeOut";
+  // const animationClass = "animate__fadeIn";
+  // useEffect(() => {
+  //   const imageTrust = document.getElementById("imageTrust");
+  //   if (imageTrust) {
+  //     imageTrust.classList.remove(animationClass);
+  //     imageTrust.classList.remove(animationClassOut);
+  //     imageTrust.classList.add(animationClass);
+  //   }
+  //   const next = (trustPosition + 1) % trustImages.length;
+  //   const move = setTimeout(() => {
+  //     if (imageTrust) {
+  //       imageTrust.classList.remove(animationClass);
+  //       imageTrust.classList.remove(animationClassOut);
+  //       imageTrust.classList.add(animationClassOut);
+  //       setTimeout(() => {
+  //         setTrustPosition(next);
+  //       }, 801);
+  //     }
+  //   }, 700000);
+  //   return () => clearTimeout(move);
+  // }, [trustPosition, trustImages]);
 
   return (
     <div className="trustContainer">
-      <div className="imageContainer">
-        <img
-          className="animate__animated"
-          id="imageTrust"
-          alt="personal de Delta"
-          src={trustImages[trustPosition]}
-        />
+      <img
+        className="animate__animated one"
+        id="imageTrust"
+        alt="personal de Delta"
+        src={trustImages[0]}
+      />
+      <img
+        className="animate__animated two"
+        id="imageTrust"
+        alt="personal de Delta"
+        src={trustImages[1]}
+      />
+      <div className="Trust">
+        <p>¿Por qué confiar en nosotros?</p>
       </div>
-      <div className="textContainer">
-        <div className="Title">
-          <p>¿Por qué confiar en nosotros?</p>
-        </div>
-        <div className="confiarList">
-          {trustStatement.map((item) => {
-            return (
-              <div className="Text" style={{textAlign: "start"}}>
-                <span></span>
-                {item}
-              </div>
-            );
-          })}
-        </div>
-      </div>
+      {trustStatement.map((item) => {
+        return (
+          <div className="Point" style={{ textAlign: "start" }}>
+            <img alt="bullet point" src={derecha} />
+            <p>{item}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
